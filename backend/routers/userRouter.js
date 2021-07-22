8f4de772be381860e63f9f8926eaab8cabae704b
Shashikant Burnwal <shashikant.burnwal@gmail.com>
2021-07-22 12:23:50 +0530

    Admin users

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
-import { generateToken, isAuth } from '../utils.js';
+import { generateToken, isAuth, isAdmin } from '../utils.js';
 
  
 
@@ -85,6 +85,12 @@ userRouter.put('/profile', isAuth, expressAsyncHandler (async(req,res)=>{
             token:generateToken(updatedUser),
         })
     }
+
+}));
+
+userRouter.get ('/', isAuth, isAdmin, expressAsyncHandler(async (req, res)=>{
+    const users = await User.find({});
+    res.send (users);
 
 }))
 
